70a73aede40ee9c463f4aba591e38cf1f16e5208
M-ST <M-ST@S-MH1240>
2023-06-22 07:46:12 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Two Numbers");
+
+            //get inputs from user
+
+            Console.WriteLine("First Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second Number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //add numbers
+            int sum = num1 + num2;
+
+            //print the sum
+            Console.WriteLine("\nSum is: " + sum);
+        }
+    }
+}
